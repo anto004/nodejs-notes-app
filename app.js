9251161f28c8ec0b014d1c340b7d18609e0b425a
anto004 <anto_004@live.com>
2020-05-29 20:25:54 +0530

    Parsing command line arguments

--- app.js
+++ app.js
@@ -1,7 +1,56 @@
 const validator = require("validator");
 const getNotes = require("./notes");
 const chalk = require("chalk");
+const yargs = require("yargs");
 
-console.log(validator.isEmail("anto_004@ymail.com"));
-
-console.log(chalk`{bold Success!}`);
+yargs
+	.command({
+		command: "add",
+		desc: "Add a new note",
+		builder: {
+			title: {
+				describe: "Note title",
+				demandOption: true,
+				type: "string",
+			},
+			body: {
+				describe: "Note body",
+				demandOption: true,
+				type: "string",
+			},
+		},
+		handler: function (argv) {
+			console.log(
+				`Note title is ${chalk.blue(
+					argv.title
+				)}\nNote body is ${chalk.blue(argv.body)}`
+			);
+		},
+	})
+	.command({
+		command: "list",
+		desc: "List all the notes",
+		handler: function () {
+			console.log("List all the notes");
+		},
+	})
+	.command({
+		command: "read",
+		desc: "Read a note",
+		builder: {
+			title: {
+				describe: "Note title",
+			},
+		},
+		handler: function (argv) {
+			console.log("Reading a note", argv);
+		},
+	})
+	.command({
+		command: "delete",
+		desc: "Delete note",
+		handler: function () {
+			console.log("Deleting a note");
+		},
+	})
+	.help().argv;
